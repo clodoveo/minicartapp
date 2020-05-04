e53bd75ae573cf9ec793e377ebd25a39df47133c
filippomaschietto <filippo@ramointeractive.com>
2020-05-04 19:15:19 +0200

    Initial commit

---
+++ www/precache-manifest.8333fe183ec22e7153d7ca305f29eadd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b3a1d316bd0e69a1fb10",
+    "url": "/static/js/main.b3a1d316.chunk.js"
+  },
+  {
+    "revision": "4e711f45e746f5235922",
+    "url": "/static/js/1.4e711f45.chunk.js"
+  },
+  {
+    "revision": "b3a1d316bd0e69a1fb10",
+    "url": "/static/css/main.de53838f.chunk.css"
+  },
+  {
+    "revision": "797b00b4a0d430038730a57212728824",
+    "url": "/index.html"
+  }
+];
---
+++ www/precache-manifest.ba11ce2c9777c4529138cff59b4b4ea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b3a1d316bd0e69a1fb10",
+    "url": "./static/js/main.b3a1d316.chunk.js"
+  },
+  {
+    "revision": "4e711f45e746f5235922",
+    "url": "./static/js/1.4e711f45.chunk.js"
+  },
+  {
+    "revision": "b3a1d316bd0e69a1fb10",
+    "url": "./static/css/main.de53838f.chunk.css"
+  },
+  {
+    "revision": "03fd23335c5116c1739166523a67b4d8",
+    "url": "./index.html"
+  }
+];
